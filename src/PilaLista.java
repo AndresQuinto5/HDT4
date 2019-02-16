e3e651361219a0ad928a3daa1696f86a6bed760a
AndresQuinto5 <42017522+AndresQuinto5@users.noreply.github.com>
2019-02-16 14:42:20 -0600

    Add files via upload

--- src/PilaLista.java
+++ src/PilaLista.java
@@ -1,2 +1,40 @@
-public class PilaLista {
+public class PilaLista<E> implements iPila<E>
+{
+    protected iLista<E> lista;
+
+    public PilaLista(iLista lista)
+    {
+        this.lista = lista;
+    }
+
+    @Override
+    public void push(E element)
+    {
+        lista.addLast(element);
+    }
+
+    @Override
+    public E pop()
+    {
+        return lista.removeLast();
+    }
+
+    @Override
+    public E peek()
+    {
+        return lista.getLast();
+    }
+
+    @Override
+    public boolean empty()
+    {
+        return lista.isEmpty();
+    }
+
+    @Override
+    public int size()
+    {
+        return lista.size();
+    }
+
 }
