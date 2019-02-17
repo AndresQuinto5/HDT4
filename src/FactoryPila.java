84d455140200f61221a8c90fb853373fffc92004
AndresQuinto5 <42017522+AndresQuinto5@users.noreply.github.com>
2019-02-16 20:07:07 -0600

    Add files via upload

---
+++ src/FactoryPila.java
@@ -0,0 +1,19 @@
+public class FactoryPila {
+
+    public iPila getImplementacion(String opc){
+
+        if("1".equals(opc)){
+            System.out.print("Opcion PilaLista Elegida");
+            return new PilaLista();
+        }
+        else if (opc.equals("2")) {
+            System.out.println("Opcion Vector Elegida");
+            return new PilaVector();
+        }
+        else if (opc.equals("3")) {
+            System.out.println("Opcion ArrayList Elegida");
+            return new PilaArraylist();
+        }
+        return null;
+    }
+}
